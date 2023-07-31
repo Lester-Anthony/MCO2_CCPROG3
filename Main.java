5e6059a64bb368716214594ff519a892d08f9bbd
LesterAnthony <lesteranthonyjr@gmail.com>
2023-08-01 03:22:28 +0800

    VM View

---
+++ Main.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        VMGUI gui = new VMGUI();
+
+        Item i1 = new Item("i1", 100, 100, 10);
+        Item i2 = new Item("i2", 100, 100, 10);
+        Item i3 = new Item("i3", 100, 100, 10);
+        Item i4 = new Item("i4", 100, 100, 10);
+        Item i5 = new Item("i5", 100, 100, 10);
+        Item i6 = new Item("i6", 100, 100, 10);
+        Item i7 = new Item("i7", 100, 100, 10);
+        Item i8 = new Item("i8", 100, 100, 10);
+        Item i9 = new Item("i9", 100, 100, 10);
+        Item i10 = new Item("i10", 100, 100, 10);
+        Item i11 = new Item("i11", 100, 100, 10);
+        Item i12 = new Item("i12", 100, 100, 10);
+        Item i13 = new Item("i13", 100, 100, 10);
+        Item i14 = new Item("i14", 100, 100, 10);
+        Item i15 = new Item("i15", 100, 100, 10);
+        Item i16 = new Item("i16", 100, 100, 10);
+        Item i17 = new Item("i17", 100, 100, 10);
+        Item i18 = new Item("i18", 100, 100, 10);
+        Item i19 = new Item("i19", 100, 100, 10);
+        Item i20 = new Item("i20", 100, 100, 10);
+        Item i21 = new Item("i21", 100, 100, 10);
+        Item i22 = new Item("i22", 100, 100, 10);
+        Item i23 = new Item("i23", 100, 100, 10);
+        Item i24 = new Item("i24", 100, 100, 10);
+        Item i25 = new Item("i25", 100, 100, 10);
+        Item i26 = new Item("i26", 100, 100, 10);
+        Item i27 = new Item("i27", 100, 100, 10);
+        Item i28 = new Item("i28", 100, 100, 10);
+        Item i29 = new Item("i29", 100, 100, 10);
+        Item i30 = new Item("i30", 100, 100, 10);
+        // Item i31 = new Item("i31", 100, 100, 10);
+        // Item i32 = new Item("i32", 100, 100, 10);
+        // Item i33 = new Item("i33", 100, 100, 10);
+     
+
+        ArrayList<Item> itemList = new ArrayList<>();
+        itemList.add(i1);
+        itemList.add(i2);
+        itemList.add(i3);
+        itemList.add(i4);
+        itemList.add(i5);
+        itemList.add(i6);
+        itemList.add(i7);
+        itemList.add(i8);
+        itemList.add(i9);
+        itemList.add(i10);
+        itemList.add(i11);
+        itemList.add(i12);
+        itemList.add(i13);
+        itemList.add(i14);
+        itemList.add(i15);
+        itemList.add(i16);
+        itemList.add(i17);
+        itemList.add(i18);
+        itemList.add(i19);
+        itemList.add(i20);
+        itemList.add(i21);
+        itemList.add(i22);
+        itemList.add(i23);
+        itemList.add(i24);
+        itemList.add(i25);
+        itemList.add(i26);
+        itemList.add(i27);
+        itemList.add(i28);
+        itemList.add(i29);
+        itemList.add(i30);
+        // itemList.add(i31);
+        // itemList.add(i32);
+        // itemList.add(i33);
+        
+
+
+
+        gui.MainMenuGUI();
+        // gui.VendingMachineGUI(itemList);
+        
+    }
+}
